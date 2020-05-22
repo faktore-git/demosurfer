90178044be5cf7d5ed5e2fde7095193667016e1c
Garvin Hicking <gh@faktor-e.de>
2020-05-22 10:42:17 +0200

    [TASK] Updates InstallTask patch to be compatible to the 2.0.2 surf release

--- surf/Packages/Libraries/faktore/typo3-surf-tasks/src/Task/Composer/InstallTask.php
+++ surf/Packages/Libraries/faktore/typo3-surf-tasks/src/Task/Composer/InstallTask.php
@@ -1,33 +1,74 @@
 <?php
 namespace Faktore\Surf\Task\Composer;
 
-/*                                                                        *
- * This script belongs to the TYPO3 project "TYPO3 Surf"                  *
- *                                                                        *
- *                                                                        */
+/*
+ * This file is part of TYPO3 Surf.
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
 
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use TYPO3\Flow\Utility\Files;
 use TYPO3\Surf\Domain\Model\Application;
 use TYPO3\Surf\Domain\Model\Deployment;
 use TYPO3\Surf\Domain\Model\Node;
+use TYPO3\Surf\Domain\Model\Task;
+use TYPO3\Surf\Domain\Service\ShellCommandServiceAwareInterface;
+use TYPO3\Surf\Domain\Service\ShellCommandServiceAwareTrait;
+use TYPO3\Surf\Exception\InvalidConfigurationException;
+use TYPO3\Surf\Exception\TaskExecutionException;
 
 /**
  * Installs the composer packages based on a composer.json file in the projects root folder
  * PATCHED: Can use a config option "composerJsonPath" to use a NON-project-root folder
  */
-class InstallTask extends \TYPO3\Surf\Task\Composer\InstallTask implements \TYPO3\Surf\Domain\Service\ShellCommandServiceAwareInterface
+class InstallTask extends Task implements ShellCommandServiceAwareInterface
 {
+    use ShellCommandServiceAwareTrait;
 
     /**
-     * @param \TYPO3\Surf\Domain\Model\Node $node
-     * @param \TYPO3\Surf\Domain\Model\Application $application
-     * @param \TYPO3\Surf\Domain\Model\Deployment $deployment
-     * @param array $options
-     * @throws \TYPO3\Surf\Exception\InvalidConfigurationException
+     * Command to run
+     *
+     * @var string
      */
-    public function execute(Node $node, Application $application, Deployment $deployment, array $options = array())
+    protected $command = 'install';
+
+    /**
+     * Arguments for the command
+     *
+     * @var array
+     */
+    protected $arguments = [
+        '--no-ansi',
+        '--no-interaction',
+        '--no-dev',
+        '--no-progress',
+        '--classmap-authoritative'
+    ];
+
+    /**
+     * Suffix for the command
+     *
+     * @var array
+     */
+    protected $suffix = ['2>&1'];
+
+    /**
+     * @param Node $node
+     * @param Application $application
+     * @param Deployment $deployment
+     * @param array $options
+     *
+     * @throws InvalidConfigurationException
+     * @throws TaskExecutionException
+     */
+    public function execute(Node $node, Application $application, Deployment $deployment, array $options = [])
     {
-        if (isset($options['useApplicationWorkspace']) && $options['useApplicationWorkspace'] === true) {
+        $options = $this->configureOptions($options);
+
+        if ($options['useApplicationWorkspace']) {
             $composerRootPath = $deployment->getWorkspacePath($application);
         } else {
             $composerRootPath = $deployment->getApplicationReleasePath($application);
@@ -41,13 +82,100 @@ class InstallTask extends \TYPO3\Surf\Task\Composer\InstallTask implements \TYPO
         if (isset($options['nodeName'])) {
             $node = $deployment->getNode($options['nodeName']);
             if ($node === null) {
-                throw new \TYPO3\Surf\Exception\InvalidConfigurationException(sprintf('Node "%s" not found', $options['nodeName']), 1369759412);
+                throw new InvalidConfigurationException(sprintf('Node "%s" not found', $options['nodeName']), 1369759412);
             }
         }
 
         if ($this->composerManifestExists($composerRootPath, $node, $deployment)) {
-            $commands = $this->buildComposerInstallCommands($composerRootPath, $options);
+            $commands = $this->buildComposerCommands($composerRootPath, $options);
             $this->shell->executeOrSimulate($commands, $node, $deployment);
         }
+    }
+
+    /**
+     * Simulate this task
+     *
+     * @param Node $node
+     * @param Application $application
+     * @param Deployment $deployment
+     * @param array $options
+     *
+     * @throws InvalidConfigurationException
+     * @throws TaskExecutionException
+     */
+    public function simulate(Node $node, Application $application, Deployment $deployment, array $options = [])
+    {
+        $this->execute($node, $application, $deployment, $options);
+    }
+
+    /**
+     * Build the composer command in the given $path.
+     *
+     * @param string $manifestPath
+     * @param array $options
+     *
+     * @return array
+     * @throws TaskExecutionException
+     */
+    private function buildComposerCommands($manifestPath, array $options)
+    {
+        $arguments = array_merge(
+            [escapeshellcmd($options['composerCommandPath']), $this->command],
+            $this->arguments,
+            array_map('escapeshellarg', $options['additionalArguments']),
+            $this->suffix
+        );
+        $script = implode(' ', $arguments);
+
+        return [
+            'cd ' . escapeshellarg($manifestPath),
+            $script,
+        ];
+    }
+
+    /**
+     * Checks if a composer manifest exists in the directory at the given path.
+     *
+     * If no manifest exists, a log message is recorded.
+     *
+     * @param string $path
+     * @param Node $node
+     * @param Deployment $deployment
+     *
+     * @return bool
+     * @throws TaskExecutionException
+     */
+    private function composerManifestExists($path, Node $node, Deployment $deployment)
+    {
+        // In dry run mode, no checkout is there, this we must not assume a composer.json is there!
+        if ($deployment->isDryRun()) {
+            return false;
+        }
+        $composerJsonPath = Files::concatenatePaths([$path, 'composer.json']);
+        $composerJsonExists = $this->shell->executeOrSimulate('test -f ' . escapeshellarg($composerJsonPath), $node, $deployment, true);
+        if ($composerJsonExists === false) {
+            $deployment->getLogger()->debug('No composer.json found in path "' . $composerJsonPath . '"');
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    protected function resolveOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired('composerCommandPath');
+
+        $resolver->setDefault('additionalArguments', [])
+            ->setNormalizer('additionalArguments', static function (Options $options, $value) {
+                return (array)$value;
+            });
+
+        $resolver->setDefault('useApplicationWorkspace', false);
+        $resolver->setDefault('nodeName', null);
+        $resolver->setAllowedTypes('additionalArguments', ['array', 'string']);
     }
 }
